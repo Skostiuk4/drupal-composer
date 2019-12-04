68c88636036cded7f3d8ada2a5f151b455abd1b6
Skostiuk4 <Skostiuk4@gmail.com>
2019-12-04 11:02:28 +0200

    Added the css and js libraries to the study module

---
+++ web/modules/custom/study/assets/js/study.js
@@ -0,0 +1,11 @@
+(function ($, Drupal, settings) {
+
+  "use strict";
+
+  Drupal.behaviors.study = {
+    attach: function (context) {
+      alert('Study');
+    }
+  }
+
+})(jQuery, Drupal, drupalSettings);
