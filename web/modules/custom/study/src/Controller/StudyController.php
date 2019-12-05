37b2c462e5777149bdebdbe63c0c2d7ab352f0cc
Skostiuk4 <Skostiuk4@gmail.com>
2019-12-05 17:41:04 +0200

    Create a sub-theme based on a Classy theme called Unique

--- web/modules/custom/study/src/Controller/StudyController.php
+++ web/modules/custom/study/src/Controller/StudyController.php
@@ -1,4 +1,19 @@
+<?php
+namespace Drupal\Study\Controller;
+use Drupal\Core\Controller\ControllerBase;
+class StudyController extends ControllerBase {
 
+  public function study() {
+    $output = array();
+
+    $output['#title'] = 'Study page title';
+
+    $output['#markup'] = 'Study!';
+
+    return $output;
+  }
+
+}
 
 
 
