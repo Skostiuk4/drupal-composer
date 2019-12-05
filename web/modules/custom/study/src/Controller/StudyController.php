489cf5e47463c6e8d00c2e088ae6cc693957f407
Skostiuk4 <Skostiuk4@gmail.com>
2019-12-05 09:15:14 +0200

    Corrected the study module

--- web/modules/custom/study/src/Controller/StudyController.php
+++ web/modules/custom/study/src/Controller/StudyController.php
@@ -1,12 +1,5 @@
-<?php
 
-namespace Drupal\study\Controller;
 
-class StudyController {
-  public function study() {
-    return array(
-     '#title' => 'Study',
-     '#markup' => 'This is some content.'
-    );
-  }
-}
+
+
+
